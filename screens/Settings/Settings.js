170539e3eabcaba4c876fde12a78af9938be61ae
Manit Soni <manitnsoni@gmail.com>
2020-11-15 12:43:08 -0800

    unbuggy version of Settings; added some text fields

--- screens/Settings/Settings.js
+++ screens/Settings/Settings.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { View, Button } from 'react-native'
-
+import {View, Button, Keyboard, TextInput, TouchableWithoutFeedback, StyleSheet} from 'react-native'
+// import ImagePicker from 'react-native-image-picker';
 import Firebase from '../../config/Firebase'
+import {Image} from "react-native-web";
 
 class Settings extends React.Component{
+
+    state = {
+        image: '',
+        name: '',
+        major: '',
+        classes: '',
+        bio: ''
+    }
     
     constructor() {
         super();
         this.onPressLogOut= this.onPressLogOut.bind(this)
+        //this.onPressContinue = this.onPressContinue.bind(this);
     }
 
     /** Handle logging out and reset stack */
@@ -19,16 +29,97 @@ class Settings extends React.Component{
         }
     }
 
+    /* selectImage = () => {
+        const options= {
+            noData: true
+        }
+        ImagePicker.launchImageLibrary(options, response => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker')
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error)
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton)
+            } else {
+                const source = { uri: response.uri }
+                console.log(source)
+                this.setState({
+                    image: source
+                })
+            }
+        })
+    } */
+
     render() {
         return (
-            <View>
-                <Button
-                    title="Log out"
-                    onPress={this.onPressLogOut} 
-                />
-            </View>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+                <View style={styles.container}>
+                    <Button
+                        style={styles.button}
+                        title="Log out"
+                        onPress={this.onPressLogOut}
+                    />
+                    <TextInput
+                        style={styles.inputBox}
+                        value={this.state.name}
+                        onChangeText={name => this.setState({ name })}
+                        placeholder='Name'
+                    />
+                    <TextInput
+                        style={styles.inputBox}
+                        value={this.state.major}
+                        onChangeText={major => this.setState({ major })}
+                        placeholder='Major'
+                    />
+                    <TextInput
+                        style={styles.inputBox}
+                        value={this.state.classes}
+                        onChangeText={classes => this.setState({ classes })}
+                        placeholder='Classes'
+                    />
+                    <TextInput
+                        style={styles.inputBox}
+                        value={this.state.bio}
+                        onChangeText={bio => this.setState({ bio })}
+                        placeholder='Bio'
+                    />
+                </View>
+            </TouchableWithoutFeedback>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+    },
+
+    inputBox: {
+        width: '100%',
+        margin: 10,
+        padding: 15,
+        fontSize: 16,
+        borderColor: '#d3d3d3',
+        borderBottomWidth: 1,
+        textAlign: 'left'
+    },
+
+    button: {
+        marginTop: 5,
+        marginBottom: 5,
+        paddingVertical: 15,
+        alignItems: 'center',
+        backgroundColor: '#F6820D',
+        borderColor: '#F6820D',
+        borderWidth: 1,
+        borderRadius: 5,
+        width: 150,
+        textAlign: 'center',
+        fontSize: 15
+    }
+
+})
 
 export default Settings
