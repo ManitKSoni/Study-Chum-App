4c82fd2a33a34610a147967c9ea656a710ed4f73
kevinlee-2000 <kelee@ucsd.edu>
2020-11-29 01:41:45 -0800

    supress warnings

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 
 import React from 'react';
-import { StyleSheet, View, LogBox } from 'react-native';
+import { StyleSheet, View} from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -79,7 +79,6 @@ export default class App extends React.Component {
   }
 
   render() {
-    LogBox.ignoreAllLogs(); //Supresses all warnings
     if (this.state.fontsLoaded && this.state.imagesLoaded) {
       return (
         <NavigationContainer>
